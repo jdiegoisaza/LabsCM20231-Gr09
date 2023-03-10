ba94d032ba93fb662e5b3b6b975aa288bfc82585
jdiegoisaza <jdiego.isaza@udea.edu.co>
2023-03-10 17:01:35 -0500

    Inicializacion del proyecto

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20231/lab1/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr09_20231.lab1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
