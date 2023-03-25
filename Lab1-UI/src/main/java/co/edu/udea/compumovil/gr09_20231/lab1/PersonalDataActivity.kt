7c30ab10fc90f2f7abeb8f2499f149af3607c54f
YennyE <yenny.tombe@udea.edu.co>
2023-03-24 20:14:20 -0500

    Creación de los campos requeridos para la información personal

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20231/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20231/lab1/PersonalDataActivity.kt
@@ -3,9 +3,9 @@ package co.edu.udea.compumovil.gr09_20231.lab1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_personal_data)
     }
 }
