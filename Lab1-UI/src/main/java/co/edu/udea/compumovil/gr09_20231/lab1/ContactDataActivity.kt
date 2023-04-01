1f4649e759869e957d8625f26a4735d4da4adcd7
jdiegoisaza <juan.diego-13@hotmail..com>
2023-04-01 02:23:16 -0500

    Vista de las 2 actividades creadas

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20231/lab1/ContactDataActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr09_20231.lab1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_data)
+    }
+}
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20231/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20231/lab1/PersonalDataActivity.kt
@@ -1,11 +1,24 @@
 package co.edu.udea.compumovil.gr09_20231.lab1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+        /*val buttonSiguiente = findViewById<Button>(R.id.buttonSiguiente)
+        buttonSiguiente.setOnClickListener {
+            // Crear un Intent para iniciar la siguiente actividad
+            val intent = Intent(this, activity_contact_data::class.java)
+
+            // Iniciar la siguiente actividad
+            startActivity(intent)
+        }*/
+
     }
+
 }
